1f8379ca995263fe30893031beb3878a72de335d
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-02-01 23:10:36 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response, body) {
+
+const url = process.argv[2] || '';
+const storage = {};
+let tmp = '';
+
+request(url, (err, res, body) => {
   if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let completed = {};
-    let tasks = JSON.parse(body);
-    for (let i in tasks) {
-      let task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
-    }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    return console.log(err);
   }
+
+  const parsedBody = JSON.parse(body);
+
+  for (let i = 0; i < parsedBody.length; i++) {
+    tmp = parsedBody[i].userId;
+    if (parsedBody[i].completed) {
+      storage[tmp] = (storage[tmp] || 0) + 1;
+    }
+  }
+
+  console.log(storage);
 });
